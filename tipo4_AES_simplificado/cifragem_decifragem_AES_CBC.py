8b1415302fa95c0692ce04725bed1e748a99ab4a
Erick Charles Neves <erick.neves.projetos@gmail.com>
2024-06-01 02:14:31 -0300

    4 - AES no modo CBC 1º cod. modificado

--- tipo4_AES_simplificado/cifragem_decifragem_AES_CBC.py
+++ tipo4_AES_simplificado/cifragem_decifragem_AES_CBC.py
@@ -1,4 +1,4 @@
-#OBS: Utilizar sage math (online) ou instalar pycrypto com as bibliotecas de criptografia, Crypto. no python. Para conseguir visualizar a sa√≠da
+#OBS: Utilizar sage math (online) ou instalar pycrypto com as bibliotecas de criptografia, Crypto. no python. Para conseguir visualizar a saída
 
 #TEE criptografia 
 
@@ -20,23 +20,24 @@ key
 iv = os.urandom(16)
 iv
 
-MSG = "encriptar e decriptar esta mensagem"
+# Solicita ao usuário que insira uma mensagem
+MSG = input("Insira a mensagem a ser encriptada:")
 
 # criptografa
-b_MSG = bytearray(MSG, encoding="ascii")  # mensagem tem que ser byte array
+b_MSG = bytearray(MSG, encoding="utf-8")  # mensagem deve ser um array de bytes
 
-# AES utilizando CBC exige que os dados sejam passados em blocos cujo tamanho seja m√∫ltiplo de 16 bytes.
+# AES utilizando CBC exige que os dados sejam passados em blocos cujo tamanho seja múltiplo de 16 bytes.
 # adequando o tamanho da mensagem original para que seja multipla de block_size definido
 block_size = 16
 n = len(b_MSG)
-spaces_add = block_size - n % block_size # calcular a qtd de espa√ßos vamos adicionar ao final da mensagem
+spaces_add = block_size - n % block_size # calcular a qtd de espaços vamos adicionar ao final da mensagem
 new_b_MSG = bytearray(MSG + ' ' * spaces_add, encoding="utf8")
 
 
 # objeto que criptografa AES com a chave gerada
 aes = algorithms.AES(key)
 
-# modo com o inicializar o vetor criado na inicializa√ß√£o
+# modo com o inicializar o vetor criado na inicialização
 cbc = modes.CBC(iv)
 
 # criar o cipher 
@@ -51,7 +52,16 @@ ct = encryptor.update(new_b_MSG) + encryptor.finalize()
 # obtem o decriptador - decifragem
 decryptor = cipher.decryptor()
 
-# decripta - decifra a mensagem para a forma original definida em MSG
-decryptor.update(ct) + decryptor.finalize()
+# Executar a operação de descriptografia e armazenar na variável
+decrypted_message = decryptor.update(ct) + decryptor.finalize()
+
+# Exibindo ás etapas em msg do processo de encriptar e decriptar
+print("\n""Mensagem encriptada concatenada na variável ct:", ct, "\n")
+print("Método embutido decodificar bytes de objeto em:", ct . decode, "\n")
+print("Objeto criptografador:", encryptor,"\n")
+print("Objeto descriptografador:", decryptor, "\n""\n")
+
+# Exibe a mensagem descriptografada e decoding utf-8 (caracteres)
+print("Mensagem inserida descriptografada:", decrypted_message . decode ('utf-8'))
 
 
